940538f7387bebbd56522855aed7a43b5e8c6d3a
jinxiangkai <jinxiangkai1935@virtuos.com.cn>
2017-04-27 16:56:32 +0800

    Signed-off-by: jinxiangkai <jinxiangkai1935@virtuos.com.cn>

---
+++ Source/Client/AndroidWarfare/Assets/BattleSystem/Map/Scripts/MapElement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapElement : MonoBehaviour
+{
+    public enum EType
+    {
+        tree,
+        land,
+        desert,
+        sea
+    }
+
+    [SerializeField]
+    protected EType _type;
+    [SerializeField]
+    protected MeshRenderer _meshRenderer;
+
+    protected virtual void OnCharacterIn()
+    {
+
+    }
+
+    protected virtual void OnCharacterOut()
+    {
+
+    }
+}
